07face1de82c7923e15c68c2807b2868c55e4c25
lvjingjing <lvjingjing@yuanqingsh.com>
2021-06-18 14:49:58 +0800

    异常接口告警等级统计解决补0问题

--- src/main/java/com/yuanqing/project/tiansu/controller/event/BusiHttpEventController.java
+++ src/main/java/com/yuanqing/project/tiansu/controller/event/BusiHttpEventController.java
@@ -39,15 +39,15 @@ public class BusiHttpEventController extends BaseController {
      * 查询接口告警列表
      */
     @GetMapping("/list")
-    @ApiOperation(value = "获取接口告警列表" , httpMethod = "GET")
-    public TableDataInfo list(@ApiParam("开始时间")@RequestParam(value = "startDate",required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startDate,
-                              @ApiParam("结束时间")@RequestParam(value = "endDate",required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endDate,
-                              @ApiParam("访问地址")@RequestParam(value = "httpUrl",required = false)String httpUrl,
-                              @ApiParam("终端IP")@RequestParam(value = "ipAddress",required = false)String ipAddress,
-                              @ApiParam("事件来源")@RequestParam(value = "eventSource",required = false)String eventSource,
-                              @ApiParam("接口执行状态")@RequestParam(value = "httpStatus",required = false)String httpStatus,
-                              @ApiParam("告警等级")@RequestParam(value = "eventLevel",required = false)String eventLevel,
-                              @ApiParam("告警状态")@RequestParam(value = "eventStatus",required = false)Integer eventStatus
+    @ApiOperation(value = "获取接口告警列表", httpMethod = "GET")
+    public TableDataInfo list(@ApiParam("开始时间") @RequestParam(value = "startDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date startDate,
+                              @ApiParam("结束时间") @RequestParam(value = "endDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") Date endDate,
+                              @ApiParam("访问地址") @RequestParam(value = "httpUrl", required = false) String httpUrl,
+                              @ApiParam("终端IP") @RequestParam(value = "ipAddress", required = false) String ipAddress,
+                              @ApiParam("事件来源") @RequestParam(value = "eventSource", required = false) String eventSource,
+                              @ApiParam("接口执行状态") @RequestParam(value = "httpStatus", required = false) String httpStatus,
+                              @ApiParam("告警等级") @RequestParam(value = "eventLevel", required = false) String eventLevel,
+                              @ApiParam("告警状态") @RequestParam(value = "eventStatus", required = false) Integer eventStatus
     ) {
         BusiHttpEvent busiHttpEvent = new BusiHttpEvent();
         busiHttpEvent.setHttpUrl(httpUrl);
@@ -65,13 +65,13 @@ public class BusiHttpEventController extends BaseController {
      * 勾选部分告警，确认状态
      */
     @PutMapping("/editSome")
-    @ApiOperation(value = "接口告警确认部分状态" , httpMethod = "PUT")
+    @ApiOperation(value = "接口告警确认部分状态", httpMethod = "PUT")
     public AjaxResult editSome(@ApiParam("勾选的需要确认状态的接口告警数据的id") @RequestBody JSONObject jsonObject) {
         String str1 = String.valueOf(jsonObject.get("ids"));
         String str = str1.substring(1, str1.length() - 1);
         String[] strArr = str.split(",");
 
-        Long[] ids = (Long[]) ConvertUtils.convert(strArr,Long.class);
+        Long[] ids = (Long[]) ConvertUtils.convert(strArr, Long.class);
         return toAjax(busiHttpEventService.updateBusiHttpEvent(ids));
     }
 
@@ -79,7 +79,7 @@ public class BusiHttpEventController extends BaseController {
      * 确认所有告警的状态
      */
     @PutMapping("/editAll")
-    @ApiOperation(value = "接口告警确认全部状态" , httpMethod = "PUT")
+    @ApiOperation(value = "接口告警确认全部状态", httpMethod = "PUT")
     public AjaxResult editAll() {
         return toAjax(busiHttpEventService.updateAllBusiHttpEvent());
     }
@@ -88,8 +88,8 @@ public class BusiHttpEventController extends BaseController {
      * 数据来源统计
      */
     @GetMapping("/getOfSources")
-    @ApiOperation(value = "获取接口告警列表" , httpMethod = "GET")
-    public AjaxResult getOfSources(@ApiParam("日期类型")@RequestParam("timeType") String timeType) {
+    @ApiOperation(value = "获取接口告警列表", httpMethod = "GET")
+    public AjaxResult getOfSources(@ApiParam("日期类型") @RequestParam("timeType") String timeType) {
         Date endDate = new Date();
         Date startDate = DateUtils.getOfTimeType(timeType);
         List<Map<String, String>> map = busiHttpEventService.statisticsOfDataSources(startDate, endDate);
@@ -100,28 +100,29 @@ public class BusiHttpEventController extends BaseController {
      * 告警等级统计
      */
     @GetMapping("/getOfEventLevel")
-    @ApiOperation(value = "获取接口告警列表" , httpMethod = "GET")
-    public AjaxResult getOfEventLevel(@ApiParam("日期类型")@RequestParam("timeType") String timeType) {
+    @ApiOperation(value = "获取接口告警列表", httpMethod = "GET")
+    public AjaxResult getOfEventLevel(@ApiParam("日期类型") @RequestParam("timeType") String timeType) {
         Date endDate = new Date();
         Date startDate = DateUtils.getOfTimeType(timeType);
         List<Map<String, String>> list = busiHttpEventService.alarmLevelStatistics(startDate, endDate);
 
         //表示一般、重要、严重的等级都有数据，直接返回就可以；否则需要补0
-        if (list.size() < 3) {
-            Map<String, String> map = new HashMap<>();
-            String level = null;
-            if (!list.contains("一般")) {
-                level = "一般";
-            } else if (!list.contains("重要")) {
-                level = "重要";
-            } else if (!list.contains("严重")) {
-                level = "严重";
-            }
-            map.put("eventLevel" , level);
-            map.put("proportion" , "0");
-            list.add(map);
+        if (list.size() != 3) {
+            list = makeUpPro(list,"一般");
+            list = makeUpPro(list,"重要");
+            list = makeUpPro(list,"严重");
         }
         return AjaxResult.success(list);
+    }
+
+    public List<Map<String, String>> makeUpPro(List<Map<String, String>> list,String level){
+        if (!list.toString().contains(level)) {
+            Map<String, String> map = new HashMap<>();
+            map.put("eventLevel", level);
+            map.put("proportion", "0");
+            list.add(map);
+        }
+        return list;
     }
 
 
