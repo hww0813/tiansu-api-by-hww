89e0e4481ffb253ec91ff4170f0a798bf334f7f9
徐灿 <1157424179@qq.com>
2021-08-19 17:29:54 +0800

    数据库脚本：添加操作行为详情字典

---
+++ src/main/resources/db/migration/V7__dict_data_focus.sql
@@ -0,0 +1,10 @@
+-- 描述：添加字典
+-- 相关功能：添加字典（操作行为详情 光圈 焦距）
+-- 更新时间：2020.8.19
+-- 作者：徐灿
+
+
+INSERT INTO `itms`.`sys_dict_data`(`dict_code`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `status`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (98, 31, '焦点前调', '31', 'oper_action_detail', NULL, NULL, 'N', '0', '', NULL, '', NULL, NULL);
+INSERT INTO `itms`.`sys_dict_data`(`dict_code`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `status`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (99, 32, '焦点后调', '32', 'oper_action_detail', NULL, NULL, 'N', '0', '', NULL, '', NULL, NULL);
+INSERT INTO `itms`.`sys_dict_data`(`dict_code`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `status`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (100, 33, '光圈扩大', '33', 'oper_action_detail', NULL, NULL, 'N', '0', '', NULL, '', NULL, NULL);
+INSERT INTO `itms`.`sys_dict_data`(`dict_code`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `status`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (101, 34, '光圈缩小', '34', 'oper_action_detail', NULL, NULL, 'N', '0', '', NULL, '', NULL, NULL);
