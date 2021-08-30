b0d1181bd6021dd0b3a5edf7f0e6b8a9e9ac6cb5
徐灿 <1157424179@qq.com>
2021-08-30 12:04:42 +0800

    consul 生产环境/开发环境配置

--- src/main/java/com/yuanqing/common/exception/config/ConfigParseException.java
+++ src/main/java/com/yuanqing/common/exception/config/ConfigParseException.java
@@ -11,6 +11,6 @@ public class ConfigParseException extends ConfigFileException {
     private static final long serialVersionUID = 1L;
 
     public ConfigParseException(String ConfigParams){
-        super("配置文件读取异常,配置项目",ConfigParams);
+        super("config.params.read.error",ConfigParams);
     }
 }
